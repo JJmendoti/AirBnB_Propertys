2e189e43c79eb75ec662865c9f4a9d6e48104314
Leonardo Caro <leocv0205@gmail.com>
2020-10-18 16:54:07 -0500

    creacion de endpoints

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -5,6 +5,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Api extends CI_Controller {
 
+    //endpoint registrar usuario
     public function signUp()
     {
         $method = $_SERVER['REQUEST_METHOD'];
@@ -14,32 +15,94 @@ class Api extends CI_Controller {
             //validar que el campo exista
             if (isset($data->type_identification) && (isset($data->identification)) && (isset($data->name)) && (isset($data->lastname)) && (isset($data->email)) && (isset($data->password))) {
             //validar que el campo no vaya vació
-                if ($data->type_identification == "" || $data->identification == "" || $data->name == "" || $data->lastname == "" || $data->email == "" || $data->password == "") {
+                if ($data->type_identification == "" || $data->identification == "" || 
+                $data->name == "" || $data->lastname == "" || $data->email == "" || 
+                $data->password == "") {
                     
                 } else {
-                    $this->UserModel->signUp($data);
+                    header('conten-type: application/json');
+                    $this->UsersModel->signUp($data);
                     $response = array('response' => 'Campos existentes, User Guardado Successfully');
                     echo json_encode($response);
                 }
             } else {
+                header('conten-type: application/json');
                 $response = array('response' => 'campos no  existente');
                 echo json_encode($response);
             }
         } else {
             header('conten-type: application/json');
             $data = array('response' => 'bad request');
+            //devuelve un json
             echo json_encode($data);
         }
 
+
     } 
 
+    public function signin(){
+        $method = $_SERVER['REQUEST_METHOD'];
+        if($method === 'POST'){
+            echo 'signin';
+        }
+        else{
+            header('content-type: application/json');
+            $response = array('response' => 'bad request');
+            echo json_encode($response);
+        }
+    }
+    public function delete(){
+        $method = $_SERVER['REQUEST_METHOD'];
+        if($method === 'DELETE'){
+            $dataJson = file_get_contents('php://input');
+            $data = json_decode($dataJson);
+            $this-> UsersModel->delete($data);
+            header('content-type: application/json');
+            $response = array('response' => 'exit delete');
+            echo json_encode($response);
+        }
+        else{
+            header('content-type: application/json');
+            $response = array('response' => 'bad request');
+            echo json_encode($response);
+        }
+    }
 
+    public function getUser(){
+        $method = $_SERVER['REQUEST_METHOD'];
+        if($method === 'GET'){
+            $users = $this-> UsersModel->getUser();
+            header('content-type: application/json');
+            $response = array('response' => 'ok', "data"=>$users);
+            echo json_encode($response);
+        }
+        else{
+            header('content-type: application/json');
+            $response = array('response' => 'bad request');
+            echo json_encode($response);
+        }
+    }
 
+    public function update(){
+        $method = $_SERVER['REQUEST_METHOD'];
+        if($method === 'PUT'){
+            $dataJson = file_get_contents('php://input');
+            $data = json_decode($dataJson);
+            $this-> UsersModel->update($data);
+            header('content-type: application/json');
+            $response = array('response' => 'exit');
+            echo json_encode($response);
+        }
+        else{
+            header('content-type: application/json');
+            $response = array('response' => 'bad request');
+            echo json_encode($response);
+        }
+    }
 
 }
 
 /* End of file Controllername.php */
-
 
 
 ?>
--- application/models/UsersModel.php
+++ application/models/UsersModel.php
@@ -4,13 +4,30 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class UsersModel extends CI_Model {
+    public function signUp($userData){
+        $this->db->insert("user",$userData);
+    }
 
    
+    public function delete($user){
+    $response=$this->db->query("DELETE FROM user WHERE id ={$user->id}");
+
+    }
+    public function getUser(){
+        $response = $this->db->query("SELECT * FROM user ")->result();
+        return $response;
+    }
+
+    public function update($user){
+        $response = $this->db->query("UPDATE user SET type_identification='{$user->type_identification}',
+        identification='{$user->identification}', name='{$user->name}',lastname='{$user->lastname}',
+        email='{$user->email}',password='{$user->password}' WHERE id={$user->id} ");
+
+    }
 
 }
 
 /* End of file UsersModel.php */
-
 
 
 ?>
